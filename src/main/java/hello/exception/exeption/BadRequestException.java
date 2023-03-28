755bb468dee78c6bfbc606fdc8e9bb159151b099
junseo <djawnstj44@naver.com>
2023-03-28 22:55:51 +0900

    com

---
+++ src/main/java/hello/exception/exeption/BadRequestException.java
@@ -0,0 +1,8 @@
+package hello.exception.exeption;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "잘못된 요청 오")
+public class BadRequestException extends RuntimeException {
+}
